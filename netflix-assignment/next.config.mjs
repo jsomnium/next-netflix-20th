e589882da9b642ba3109d4617376396a0c6a2787
yyj0917 <yyj0917@yonsei.ac.kr>
2024-11-10 21:49:24 +0900

    Fix : rewrite수정, fetch 함수 생성

--- netflix-assignment/next.config.mjs
+++ netflix-assignment/next.config.mjs
@@ -17,12 +17,18 @@ const nextConfig = {
       // TV 프로그램 데이터를 가져오는 프록시 설정
       {
         source: '/api/tv',
-        destination: `${process.env.NEXT_PUBLIC_URL}/tv/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`,
+        destination: `${process.env.NEXT_PUBLIC_URL}/tv?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`,
       },
-      // {
-      //   source: '/api/movies/:category',
-      //   destination: `${process.env.NEXT_PUBLIC_URL}/movie/:category?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`,
-      // },
+      // 영화 데이터를 카테고리별로 가져오는 프록시 설정
+      {
+        source: '/api/movies/:category',
+        destination: `${process.env.NEXT_PUBLIC_URL}/movie/:category?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`,
+      },
+      // TV 프로그램 데이터를 카테고리별로 가져오는 프록시 설정
+      {
+        source: '/api/tv/:category',
+        destination: `${process.env.NEXT_PUBLIC_URL}/tv/:category?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`,
+      },
     ];
   },
 };
